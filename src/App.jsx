6370586b307a31c36e69662331c6a3de3d8e2a40
katerinaonix8585 <katerinaonix8585@gmail.com>
2024-04-21 15:19:00 +0200

    Homework02

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
-import Lesson02 from './lessons/Lesson02/Lesson02';
+// import Lesson02 from './lessons/Lesson02/Lesson02';
+import Homework02 from './homeworks/Homework02/Homework02'; 
 
 function App() {
   return (
     <div className="App">
-      <Lesson02 />
+    {/*<Lesson02 /> */}  
+    <Homework02 />
     </div>
   );
 }
---
+++ src/homeworks/Homework02/Homework02.jsx
@@ -0,0 +1,13 @@
+import ProfileCard from "../../components/ProfileCard/ProfileCard";
+import "./styles.css"
+
+function Homework02(){
+    return (<div className="profile-card-container">
+        <ProfileCard />
+        <ProfileCard />
+        <ProfileCard />
+    </div>
+    );
+}
+
+export default Homework02;
